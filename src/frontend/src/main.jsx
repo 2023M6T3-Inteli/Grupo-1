ed4f3ec238cc1267530a3453543f3a09cca33b19
anaclaralmz <ana.zaidan@sou.inteli.edu.br>
2023-05-30 16:53:32 -0300

    update route start page

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -32,7 +32,7 @@ const router = createBrowserRouter([
     element: <Login />,
   },
   {
-    path: "/StartPage",
+    path: "/",
     element: <StartPage />,
   },
   {
@@ -70,3 +70,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <RouterProvider router={router} />
   </React.StrictMode>,
 )
+

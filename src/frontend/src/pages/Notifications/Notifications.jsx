b8f286d0fcb68d47e4d28949cb0b40a236f1bb8e
Daniel Eduardo da Silva Cunha <daniel.cunha@sou.inteli.edu.br>
2023-06-21 17:17:05 -0300

    integrated notifications - new and already seen

--- src/frontend/src/pages/Notifications/Notifications.jsx
+++ src/frontend/src/pages/Notifications/Notifications.jsx
@@ -2,6 +2,8 @@ import NavBar from '../../components/NavBar/NavBar';
 import TopBar from '../../components/TopBar/TopBar'
 import './Notifications.css';
 import CardNotification from '../../components/CardNotification/CardNotification';
+import CardAllNotification from '../../components/CardAllNotification/CardAllNotification';
+import TopNavNotifications from "../../components/TopNavNotifications/TopNavNotifications";
 import { useState, useEffect } from 'react';
 import axios from "../../../api";
 
@@ -9,6 +11,18 @@ import axios from "../../../api";
 export default function Notifications() {
 const [dados, setDados] = useState([]);
 const [loading, setLoading] = useState(true);
+  const [newNotificationsPage, setNewNotificationsPage] = useState(true);
+  const [AllNotificationsPage, setAllNotificationsPage] = useState(false);
+  
+  function changeToNewNotifications() {
+    setNewNotificationsPage(true);
+    setAllNotificationsPage(false);
+  }
+
+  function changeToAllNotifications() {
+    setNewNotificationsPage(false);
+    setAllNotificationsPage(true);
+  }
 
 useEffect(() => {
   const userId = JSON.parse(sessionStorage.getItem("user"));
@@ -31,16 +45,32 @@ if (loading) {
 }
 
   return (
-    <div className='body'>
+    <div className="body">
       <TopBar title={"Notifications"}></TopBar>
-      <div className='cards'>
-        {dados.map(item => (
-
-        <CardNotification  description={item.description}></CardNotification>
-        ))}
-      
+      <div>
+        <TopNavNotifications
+          onChangeToAll={changeToNewNotifications}
+          onChangeToMy={changeToAllNotifications}
+          newNotifications={newNotificationsPage}
+          AllNotifications={AllNotificationsPage}
+        />
       </div>
-      
+      <div className="cards">
+        {newNotificationsPage && (
+            <CardNotification
+             
+            ></CardNotification>
+     
+        )}
+
+        {AllNotificationsPage && (
+            <CardAllNotification
+           
+            ></CardAllNotification>
+
+        )}
+      </div>
+
       <NavBar></NavBar>
     </div>
   );

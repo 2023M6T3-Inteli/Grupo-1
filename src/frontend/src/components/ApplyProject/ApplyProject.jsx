48d013814a68feea91e8e14a3b88954f09ee33f3
Pedro Gattai <Pedro.Gattai@sou.inteli.edu.br>
2023-06-22 15:24:04 -0300

    feat
    modal apply

--- src/frontend/src/components/ApplyProject/ApplyProject.jsx
+++ src/frontend/src/components/ApplyProject/ApplyProject.jsx
@@ -1,22 +1,28 @@
 import React, { useState } from "react";
 import "./ApplyProject.css";
 
+import ApplySuccess from "../ApplySuccess/ApplySuccess";
+
 export default function Modal() {
   const [modal, setModal] = useState(true); // Alterado para true
+  const [successModal, setSuccessModal] = useState(false);
 
   const toggleModal = () => {
     setModal(!modal);
   };
 
+  const toggleSuccessModal = () => {
+    setSuccessModal(!successModal);
+  };
+
   if (modal) {
-    document.body.classList.add('active-modal')
+    document.body.classList.add("active-modal");
   } else {
-    document.body.classList.remove('active-modal')
+    document.body.classList.remove("active-modal");
   }
 
   return (
     <>
-      {/* Removido o botão "Open" */}
       {modal && (
         <div className="modal">
           <div onClick={toggleModal} className="overlay"></div>
@@ -24,31 +30,35 @@ export default function Modal() {
             <h2 className="apply-project">Apply to project</h2>
             <div className="line"></div>
             <div className="select-roles-text">
-              <p className="select-roles"> Select the roles you are interested in: </p>
+              <p className="select-roles">Select the roles you are interested in:</p>
             </div>
             <div className="roles">
               <div>
-                <input className="frontend " type="checkbox" name="role" value="frontend" />
-                <label htmlFor="frontend">Frontend</label> {/* Alterado for para htmlFor */}
+                <input className="frontend" type="checkbox" name="role" value="frontend" />
+                <label htmlFor="frontend">Frontend</label>
               </div>
               <div>
-                <input className="ux" type="checkbox" name="role" value="ux" /> <label
-                  htmlFor="ux">UX Designer</label> {/* Alterado for para htmlFor */}
+                <input className="ux" type="checkbox" name="role" value="ux" />{" "}
+                <label htmlFor="ux">UX Designer</label>
               </div>
               <div>
-                <input className="data" type="checkbox" name="role" value="data" /> <label
-                  htmlFor="data">Data Analyst</label> {/* Alterado for para htmlFor */}
+                <input className="data" type="checkbox" name="role" value="data" />{" "}
+                <label htmlFor="data">Data Analyst</label>
               </div>
               <div>
-                <button className="apply" type="submit">Apply</button>
+                <button className="apply" type="submit" onClick={toggleSuccessModal}>
+                  Apply
+                </button>
               </div>
             </div>
             <button className="close-modal" onClick={toggleModal}>
-              <span className="close-modal-text"> X </span>
+              <span className="close-modal-text">X</span>
             </button>
           </div>
         </div>
       )}
+
+      {successModal && <ApplySuccess togglePreviousModal={toggleModal} toggleModal={toggleSuccessModal} />}
     </>
   );
 }
--- src/frontend/src/components/ApplySuccess/ApplySuccess.jsx
+++ src/frontend/src/components/ApplySuccess/ApplySuccess.jsx
@@ -1,45 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import "./ApplySuccess.css";
 import success from "../../assets/success.svg";
 
-export default function Modal() {
-  const [modal, setModal] = useState(false);
+export default function ApplySuccess({ toggleModal, togglePreviousModal }) {
+  useEffect(() => {
+    document.body.classList.add("active-modal");
 
-  const toggleModal = () => {
-    setModal(!modal);
+    return () => {
+      document.body.classList.remove("active-modal");
+    };
+  }, []);
+
+  const handlePreviousModal = () => {
+    togglePreviousModal();
+    toggleModal();
   };
 
-  if(modal) {
-    document.body.classList.add('active-modal')
-  } else {
-    document.body.classList.remove('active-modal')
-  }
-
   return (
-    <>
-      <button onClick={toggleModal} className="btn-modal">
-        Open
-      </button>
-
-      {modal && (
-        <div className="modal">
-          <div onClick={toggleModal} className="overlay"></div>
-          <div className="modal-content">
-            <div className="line"></div>
-            <div className="check-img">
-              <img src= {success} className="check" />
-            </div>
-            <p className="congratulations"> <strong>Congratulations!</strong> </p>
-            <p className="check-text"> Your project has been <br /> sent.</p>
-            <button className="close-modal" onClick={toggleModal}>
-              <span className="close-modal-text"> X </span> 
-            </button>
-          </div>
+    <div className="modal">
+      <div onClick={handlePreviousModal} className="overlay"></div>
+      <div className="modal-content">
+        <div className="line"></div>
+        <div className="check-img">
+          <img src={success} alt="success" className="check" />
         </div>
-      )}
-      
-    </>
+        <p className="congratulations">
+          <strong>Congratulations!</strong>
+        </p>
+        <p className="check-text">
+          Your project has been <br /> sent.
+        </p>
+        <button className="close-modal" onClick={handlePreviousModal}>
+          <span className="close-modal-text">X</span>
+        </button>
+      </div>
+    </div>
   );
 }
-
-

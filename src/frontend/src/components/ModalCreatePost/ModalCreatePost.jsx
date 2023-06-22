9eb0a7740b6303136a1331318cda3c693f87986d
jordan.acs <jordan.silva@sou.inteli.edu.br>
2023-06-22 01:53:11 -0300

    Ajuste no front
    FIx Project Page
    Fix NavBar
    Link faqPage and notificationPage ( o ícone de sino tá feio por enquanto

--- src/frontend/src/components/ModalCreatePost/ModalCreatePost.jsx
+++ src/frontend/src/components/ModalCreatePost/ModalCreatePost.jsx
@@ -1,6 +1,5 @@
 
-import React, { useState, useEffect } from "react";
-import React, { useState,useEffect } from "react";
+import  { useState, useEffect } from "react";
 
 import "./ModalCreatePost.css";
 import axios from 'axios';
